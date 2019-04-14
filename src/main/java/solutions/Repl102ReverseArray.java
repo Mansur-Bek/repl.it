3e7be276f4179632102b652738f76f6064380e8c
Vasya Fomiuk <vasya23fomiuk@gmail.com>
2019-04-14 10:29:54 -0400

    Zombie

---
+++ src/main/java/solutions/Repl102ReverseArray.java
@@ -0,0 +1,30 @@
+package solutions;
+
+import java.util.Arrays;
+
+public class Repl102ReverseArray {
+	public static void main(String[] args) {
+		String sentence = "Java is fun";
+		String[] words = sentence.split(" ");
+//		for (String word : words) {
+//			System.out.println(word);
+//		}
+		System.out.println(Arrays.toString(words));
+		System.out.println();
+		
+		
+		
+		
+		for (int index = words.length-1; index >= 0; index--) {
+			System.out.println(words[index]);
+		}
+		
+		
+		
+		System.out.println();
+		for(int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
+		}
+
+	}
+}
---
+++ src/main/java/solutions/ZoombieAttack1and2.java
@@ -0,0 +1,60 @@
+package solutions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZoombieAttack1and2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+//		int[] inhabitants = { 3, 6, 0, 4, 3, 2, 7, 0 };
+		int[] inhabitants = { 0, 20, 0, 20, 20, 10, 0, 10 };
+//		int[] inhabitants = { 10, 0, 20, 4, 0, 1, 6, 0 };
+//		for (int i = 0; i < inhabitants.length; i++) {
+//			inhabitants[i] = input.nextInt();
+//		}
+//		[0, 0, 0, 0, 0, 1, 0, 0]
+		// TODO. Write you code below this line.
+		int day = 0;
+		int sum = 1;
+		boolean changedPrevious = false;
+		System.out.println("Day " + day++ + " " + Arrays.toString(inhabitants));
+		while (sum != 0) {
+			sum = 0;
+			for (int index = 0; index < inhabitants.length; index++) {
+				if (index >= inhabitants.length - 1) {
+					if (inhabitants[index - 1] == 0) {
+						inhabitants[index] = inhabitants[index] / 2;
+					}
+				} else if (index == 0) {
+					if (inhabitants[index + 1] == 0) {
+						inhabitants[index] = inhabitants[index] / 2;
+					}
+				} else {
+					if (inhabitants[index] != 0) {
+						if (inhabitants[index + 1] == 0) {
+							inhabitants[index] = inhabitants[index] / 2;
+						} else if (inhabitants[index - 1] == 0 && !changedPrevious) {
+							inhabitants[index] = inhabitants[index] / 2;
+							if (inhabitants[index] == 0)
+								changedPrevious = true;
+						}
+					}
+				}
+			}
+			changedPrevious = false;
+			sum = 0;
+			for (int p : inhabitants) {
+				sum += p;
+			}
+			System.out.println("Day " + day++ + " " + Arrays.toString(inhabitants));
+			if (sum == 0) {
+				break;
+			}
+			sum = 1;
+		}
+		System.out.println("---- EXTINCT ----");
+	}
+
+//	
+}
