dd6f4321b2fa42b1303deb82103af4be6656e39c
Vasya Fomiuk <vasya23fomiuk@gmail.com>
2019-04-18 11:03:35 -0400

    update

---
+++ src/main/java/solutions/ChessBoard.java
@@ -0,0 +1,18 @@
+package solutions;
+
+import java.util.Arrays;
+
+public class ChessBoard {
+	public static void main(String[] args) {
+		String[][] chessBoard = new String[8][8];
+		int counter = 0;
+		for(int i=0; i < 8; i++) {
+			for(char j = 'a'; j <='h' ; j++) {
+				chessBoard[i][counter++] = (i+1)+String.valueOf(j);
+			}
+			counter=0;
+		}
+		// DO NOT CHANGE
+		System.out.println(Arrays.deepToString(chessBoard));
+	}
+}
