de95830baaebc7596f6e8122838f61ceccbaae4a
Vasya Fomiuk <vasya23fomiuk@gmail.com>
2019-04-25 00:21:01 -0400

    repl

---
+++ src/main/java/solutions/Repl_134_2D_Arrays_Declare_and_Create.java
@@ -0,0 +1,13 @@
+package solutions;
+
+import java.util.Arrays;
+
+public class Repl_134_2D_Arrays_Declare_and_Create {
+	public static void main(String[] args) {
+		// WRITE YOUR CODE HERE
+		int[][] values = { { 8, 20, 50 }, { 12, 30, 75 } };
+
+		// DO NOT CHANGE
+		System.out.println(Arrays.deepToString(values));
+	}
+}
